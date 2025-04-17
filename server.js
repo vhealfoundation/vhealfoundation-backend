36fbfa7cc1ca408111dff409dde75471e1ad63dc
dymphnamedals <dymphnamedals@gmail.com>
2025-04-17 09:53:39 +0530

    tested env

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ connectDatabase();
 app.get('/', (req, res) => {
     res.send('Server is Running! 🚀');
 });
+app.get("/env-check", (req, res) => {
+    res.json({
+      donation_id: process.env.DONATION_RAZORPAY_KEY_ID,
+      appointment_id: process.env.APPOINTMENT_RAZORPAY_KEY_ID,
+    });
+  });
+  
 
 
 app.listen(PORT, () => {
